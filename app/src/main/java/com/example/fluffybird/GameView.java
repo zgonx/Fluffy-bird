5bc7476fec9094d68c98d641488fbe144ec2d278
p4we1m <polandproplayer@gmail.com>
2023-04-18 10:32:20 +0200

    Dodanie licznika wyniku, przycisku start i rur.

--- app/src/main/java/com/example/fluffybird/GameView.java
+++ app/src/main/java/com/example/fluffybird/GameView.java
@@ -1,6 +1,7 @@
 package com.example.fluffybird;
 
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
@@ -18,17 +19,23 @@ public class GameView extends View {
     private Bird bird;
     private android.os.Handler handler;
     private Runnable r;
+    private ArrayList<Pipe> arrPipes;
+    private int sumpipe, distance;
+    private int score, bestscore = 0;
+    private boolean start;
+    private Context context;
     public GameView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
-        bird = new Bird();
-        bird.setWidth(100*Constants.SCREEN_WIDTH/1080);
-        bird.setHeight(100*Constants.SCREEN_HEIGHT/1920);
-        bird.setX(100*Constants.SCREEN_WIDTH/1080);
-        bird.setY(Constants.SCREEN_HEIGHT/2-bird.getHeight()/2);
-        ArrayList<Bitmap> arrBms = new ArrayList<>();
-        arrBms.add(BitmapFactory.decodeResource(this.getResources(),R.drawable.bird1));
-        arrBms.add(BitmapFactory.decodeResource(this.getResources(),R.drawable.bird2));
-        bird.setArrBms(arrBms);
+        this.context = context;
+        SharedPreferences sp = context.getSharedPreferences("gamesetting",Context.MODE_PRIVATE);
+        if(sp!=null) {
+            bestscore = sp.getInt("bestscore",0);
+        }
+        score = 0;
+        bestscore = 0;
+        start = false;
+        initBird();
+        initPipe();
         handler = new Handler();
         r = new Runnable() {
             @Override
@@ -38,9 +45,79 @@ public class GameView extends View {
         };
 
     }
+
+    private void initPipe() {
+        sumpipe = 6;
+        distance = 300*Constants.SCREEN_HEIGHT/1920;
+        arrPipes = new ArrayList<>();
+        for (int i = 0; i < sumpipe; i++) {
+            if(i<sumpipe/2) {
+                this.arrPipes.add(new Pipe(Constants.SCREEN_WIDTH+i*((Constants.SCREEN_WIDTH+200*Constants.SCREEN_WIDTH/1080)/(sumpipe/2)),
+                        0, 200*Constants.SCREEN_WIDTH/1080, Constants.SCREEN_HEIGHT/2));
+                this.arrPipes.get(this.arrPipes.size()-1).setBm(BitmapFactory.decodeResource(this.getResources(), R.drawable.pipe2));
+                this.arrPipes.get(this.arrPipes.size()-1).randomY();
+            } else {
+                this.arrPipes.add(new Pipe(this.arrPipes.get(i-sumpipe/2).getX(), this.arrPipes.get(i-sumpipe/2).getY()
+                        +this.arrPipes.get(i-sumpipe/2).getHeight() + this.distance, 200*Constants.SCREEN_WIDTH/1080, Constants.SCREEN_HEIGHT/2));
+                this.arrPipes.get(this.arrPipes.size()-1).setBm(BitmapFactory.decodeResource(this.getResources(), R.drawable.pipe1));
+            }
+        }
+    }
+
+    private void initBird() {
+        bird = new Bird();
+        bird.setWidth(100*Constants.SCREEN_WIDTH/1080);
+        bird.setHeight(100*Constants.SCREEN_HEIGHT/1920);
+        bird.setX(100*Constants.SCREEN_WIDTH/1080);
+        bird.setY(Constants.SCREEN_HEIGHT/2-bird.getHeight()/2);
+        ArrayList<Bitmap> arrBms = new ArrayList<>();
+        arrBms.add(BitmapFactory.decodeResource(this.getResources(),R.drawable.bird1));
+        arrBms.add(BitmapFactory.decodeResource(this.getResources(),R.drawable.bird2));
+        bird.setArrBms(arrBms);
+    }
+
     public void draw(Canvas canvas){
         super.draw(canvas);
-        bird.draw(canvas);
+        if(start) {
+            bird.draw(canvas);
+            for (int i = 0; i <sumpipe; i++) {
+                if(bird.getRect().intersect(arrPipes.get(i).getRect())||bird.getY()-bird.getHeight()<0||bird.getY()>Constants.SCREEN_HEIGHT){
+                    Pipe.speed = 0;
+                    MainActivity.txt_score_over.setText(MainActivity.txt_score.getText());
+                    MainActivity.txt_best_score.setText("Rekord: " + bestscore);
+                    MainActivity.txt_score.setVisibility(INVISIBLE);
+                    MainActivity.rl_game_over.setVisibility(VISIBLE);
+                }
+                if(this.bird.getX()+this.bird.getWidth()>arrPipes.get(i).getX()+arrPipes.get(i).getWidth()/2
+                        &&this.bird.getX()+this.bird.getWidth()<=arrPipes.get(i).getX()+arrPipes.get(i).getWidth()/2+Pipe.speed
+                        &&i<sumpipe/2) {
+                    score++;
+                    if(score>bestscore) {
+                        bestscore = score;
+                        SharedPreferences sp = context.getSharedPreferences("gamesetting",Context.MODE_PRIVATE);
+                        SharedPreferences.Editor editor = sp.edit();
+                        editor.putInt("bestscore", bestscore);
+                        editor.apply();
+                    }
+                    MainActivity.txt_score.setText(""+score);
+                }
+                if(this.arrPipes.get(i).getX() < -arrPipes.get(i).getWidth()) {
+                    this.arrPipes.get(i).setX(Constants.SCREEN_WIDTH);
+                    if(i < sumpipe/2) {
+                        arrPipes.get(i).randomY();
+                    } else {
+                        arrPipes.get(i).setY(this.arrPipes.get(i-sumpipe/2).getY()
+                                + this.arrPipes.get(i-sumpipe/2).getHeight() + this.distance);
+                    }
+                }
+                this.arrPipes.get(i).draw(canvas);
+            }
+        }else {
+            if(bird.getY()>Constants.SCREEN_HEIGHT/2) {
+                bird.setDrop(-15*Constants.SCREEN_HEIGHT/1920);
+            }
+            bird.draw(canvas);
+        }
         handler.postDelayed(r, 10);
 
     }
@@ -51,5 +128,20 @@ public class GameView extends View {
             bird.setDrop(-15);
         }
         return true;
+    }
+
+    public boolean isStart() {
+        return start;
+    }
+
+    public void setStart(boolean start) {
+        this.start = start;
+    }
+
+    public void reset() {
+        MainActivity.txt_score.setText("0");
+        score=0;
+        initPipe();
+        initBird();
     }
 }
--- app/src/main/java/com/example/fluffybird/MainActivity.java
+++ app/src/main/java/com/example/fluffybird/MainActivity.java
@@ -4,9 +4,17 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.DisplayMetrics;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    public static TextView txt_score, txt_best_score, txt_score_over;
+    public static RelativeLayout rl_game_over;
+    public static Button btn_start;
+    private GameView gv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,5 +25,28 @@ public class MainActivity extends AppCompatActivity {
         Constants.SCREEN_HEIGHT = dm.heightPixels;
         Constants.SCREEN_WIDTH = dm.widthPixels;
         setContentView(R.layout.activity_main);
+        txt_score = findViewById(R.id.txt_score);
+        txt_best_score = findViewById(R.id.txt_best_score);
+        txt_score_over = findViewById(R.id.txt_score_over);
+        rl_game_over = findViewById(R.id.rl_game_over);
+        btn_start = findViewById(R.id.btn_start);
+        gv = findViewById(R.id.gv);
+        btn_start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                gv.setStart(true);
+                txt_score.setVisibility(View.VISIBLE);
+                btn_start.setVisibility(View.INVISIBLE);
+            }
+        });
+        rl_game_over.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                btn_start.setVisibility(View.VISIBLE);
+                rl_game_over.setVisibility(View.INVISIBLE);
+                gv.setStart(false);
+                gv.reset();
+            }
+        });
     }
 }
